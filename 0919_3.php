da59f8106066354ee87c61422c2555c8da1a32ba
o2965905 <o2965905@gmail.com>
2022-09-19 18:10:46 +0800

    if else判斷式

---
+++ 0919_3.php
@@ -0,0 +1,17 @@
+<!-- if else 判斷 -->
+<?php
+//試著將 true 變成 false,0,1 分別看結果為何
+
+// $a=true;
+// $a=false;
+// $a=1;
+$a=0;
+
+if($a){
+    echo "a 是 true 結果";
+}else{
+    echo "a 是 false 結果";
+}
+
+
+?>
